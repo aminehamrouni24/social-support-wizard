a5352b1fffcba01bc4c6a07587dc269b5c30e6b1
XPRISTO <aminhamrouni82@gmail.com>
2026-05-30 21:01:28 +0200

    social-support-portal

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import './i18n'; // ✅ Add this — must initialize before app renders
 import AppRoot from './AppRoot';
 import { createRoot } from 'react-dom/client';
-
+// ****************************
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppRoot />
